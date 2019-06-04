19aa689f21c7cdcb2eb02e12cdc29554e2fdb1aa
cjy <840191129@qq.com>
2019-06-04 16:35:22 +0800

    5.0

--- src/Hello2.java
+++ src/Hello2.java
@@ -2,6 +2,6 @@ public class Hello2 {
     /*6.4 7.0*/
     public static void main(String[] args) {
         System.out.println("hello2");
-        System.out.println("genxin");
+        System.out.println("genxin1");
     }
 }
